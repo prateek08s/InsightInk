613d4fe9034a82fa648c3c8f7dbc05b6a44b0c82
Prateek08s <Prateek.sachan2020@vitstudent.ac.in>
2024-07-27 13:35:51 +0530

    Describe the changes you made

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import google.generativeai as genai
+import faiss
 
 # Load environment variables
 load_dotenv()
